d9ea3198dc4b798f9c28e259a356e80ae6079031
Cristiano Calcagno <ccrisccris@gmail.com>
2020-05-09 03:55:17 +0200

    more Windows pleasing

---
+++ examples/termination/termination.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const child_process = require("child_process");
+const path = require("path");
+
+const spawn = child_process.spawn;
+
+const isWindows = /^win/i.test(process.platform);
+
+const reanalyzeNativePath = path.join(
+  __dirname,
+  "../../_esy/default/build/install/default/bin/reanalyze.exe"
+);
+
+const shell = isWindows ? true : false;
+
+child_process.spawnSync(reanalyzeNativePath, ["-exception", "-ci", "-debug"], {
+  stdio: ["inherit", "inherit"],
+  shell,
+});
