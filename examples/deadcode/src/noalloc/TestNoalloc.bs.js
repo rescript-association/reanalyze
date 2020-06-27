88d8cc4299229a9c7fd4fa29c9015707ff72c663
Cristiano Calcagno <ccrisccris@gmail.com>
2020-06-27 09:12:45 +0200

    rearrange examples

--- examples/deadcode/src/noalloc/TestNoalloc.bs.js
+++ examples/deadcode/src/noalloc/TestNoalloc.bs.js
@@ -56,77 +56,6 @@ function unpair2(v) {
   return v[0] + v[1] | 0;
 }
 
-function sumVec(v) {
-  var match = v[1];
-  var match$1 = v[0];
-  return [
-          match$1[0] + match[0] | 0,
-          match$1[1] + match[1] | 0
-        ];
-}
-
-function scale(s) {
-  return [
-          [
-            s,
-            1.0,
-            1.0
-          ],
-          [
-            1.0,
-            s,
-            1.0
-          ],
-          [
-            1.0,
-            1.0,
-            s
-          ]
-        ];
-}
-
-function rotation(a) {
-  return [
-          [
-            0.0,
-            -1.0 * a,
-            0.0
-          ],
-          [
-            a,
-            0.0,
-            0.0
-          ],
-          [
-            0.0,
-            0.0,
-            a
-          ]
-        ];
-}
-
-function mulVecVec(v1, v2) {
-  var x = v1[0] * v2[0];
-  var y = v1[1] * v2[1];
-  var z = v1[2] * v2[2];
-  return x + y + z;
-}
-
-function mulMatVec(m, v) {
-  var x = mulVecVec(m[0], v);
-  var y = mulVecVec(m[1], v);
-  var z = mulVecVec(m[2], v);
-  return [
-          x,
-          y,
-          z
-        ];
-}
-
-function restMatrix(v) {
-  return mulMatVec(rotation(0.123), mulMatVec(scale(2.0), v));
-}
-
 function id(x) {
   return x;
 }
@@ -167,12 +96,6 @@ export {
   quad ,
   fl ,
   unpair2 ,
-  sumVec ,
-  scale ,
-  rotation ,
-  mulVecVec ,
-  mulMatVec ,
-  restMatrix ,
   id ,
   id2 ,
   y ,
